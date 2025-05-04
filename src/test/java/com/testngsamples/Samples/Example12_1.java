0faf8c659e64e406e5b4fa8e6f696e9773d1898d
Ish Jaiswal <ishjaiswal7@gmail.com>
2025-05-04 10:41:36 +0530

    Example12_1 added

---
+++ src/test/java/com/testngsamples/Samples/Example12_1.java
@@ -0,0 +1,18 @@
+package com.testngsamples.Samples;
+
+import org.testng.annotations.Test;
+
+// Parallel Test Execution in TestNG
+public class Example12_1 {
+	
+	@Test
+	public void FacultyLogin() {
+		System.out.println("Faculty Login");
+	}
+
+	@Test
+	public void FacultyDetails() {
+		System.out.println("Faculty Details");
+	}
+	
+}
